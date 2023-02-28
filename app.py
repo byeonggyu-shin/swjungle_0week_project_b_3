334df4b8690eef12c45a620c3481fab77a40f4d4
Gichani <thdrlcks97@gmail.com>
2023-02-28 22:06:09 +0900

    fix: api key값 추가

--- app.py
+++ app.py
@@ -1,14 +1,48 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
 
 #Atlas DB 접속
-load_dotenv()
-ID = os.getenv("ID")
-PW = os.getenv("PW")
-client = MongoClient(f'mongodb+srv://{ID}:{PW}@cluster0.30nb9z3.mongodb.net/?retryWrites=true&w=majority', 27017) 
+client = MongoClient('localhost', 27017) 
 db = client.team_3
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# #로그인
+# @app.route('/users/me', methods=['GET', 'POST'])
+# def hello(name=None):
+#     return render_template('hello.html', name=name)
+# #회원가입
+# @app.route('/user/sign_in', methods=['POST'])
+# def hello(name=None):
+#     return render_template('hello.html', name=name)
+# #팀원 전체 조회
+# @app.route('/user/get', methods=['GET'])
+# def hello(name=None):
+#     return render_template('hello.html', name=name)
+#상세 정보 조회
+@app.route('/user/d_get', methods=['GET'])
+def hello(name=None):
+    return render_template('index.html', name=name)
+# #프로필 수정
+
+@app.route('/user/<userId>/fix_profile', methods=['POST'])
+def hello(userId, name, phone, github, email, where, about):
+    db.userId.update({'userId':userId}, {{'name':name,
+                                          'phone':phone,
+                                          'github':github,
+                                          'email':email,
+                                          'where':where,
+                                          'about':about}})
+    response = request.form({'userId':userId})
+    return jsonify(response)
+#프로필 삭제
+# @app.route('/user/delete', methods=['POST'])
+# def hello(name=None):
+#     return render_template('hello.html', name=name)
 
 
 app = Flask(__name__)
