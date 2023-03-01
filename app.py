3d4cb88bee8c31d986c6dadb84fb55a43ad2edf5
seojuntaek <jwutn908@gmail.com>
2023-03-01 23:28:13 +0900

    login 수정

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, jsonify, request, redirect, url_for
-from flask_login import LoginManager, login_user
+from flask import Flask, render_template, jsonify, request, redirect, url_for, session
+# from flask_login import LoginManager, login_user, login_required
+# from user import User
 from pymongo import MongoClient
 import os
 import hashlib
@@ -9,14 +10,24 @@ from time import time, gmtime
 # from datetime import datetime
 
 app = Flask(__name__)
+# login_manager = LoginManager()
+# login_manager.init_app(app)
 
 #Atlas DB 접속
 client = MongoClient('localhost', 27017) 
 db = client.team_3
 
+app.secret_key = 'My_Key'
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
+# @app.route('/profile')
+# @login_required
+# def profile():
+#     # 로그인된 사용자만 접근 가능한 페이지
+#     return 'Welcome to your profiile page'
 
 # 메인 페이지
 @app.route('/main')
@@ -50,6 +61,42 @@ def signup():
         count += 1
    return jsonify({'result': 'success'})
 
+# @login_manager.user_loader
+# def load_user(user_id):
+#     # 사용자 ID로 사용자 정보를 가져오는 함수
+#     return User.get(user_id)
+
+# @app.route('/login', methods=['POST'])
+# def login():
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         print('{}가 로그인 했습니다.'.format(form.data.get('userid')))
+#         session['userId'] = form.data.get('userid')
+#         return redirect('/')
+#     return render_template('login.html', form=form)
+
+# @app.route('/main')
+# def main():
+#     if 'id' in session:
+#         id = session['id']
+#         print(id)
+#         return render_template("main.html")
+#     else:
+#         return render_template("login.html")
+
+# @app.route('/login', methods = ['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         session['id'] = request.form['id']
+#         return render_template("main.html")
+#     else:
+#         return render_template("login.html")
+
+# @app.route('/logout')
+# def logout():
+#     session.pop('id', None)
+#     return render_template("main.html")
+
 SECRET_KEY = 'secret_key'
 @app.route('/users/me', methods=['POST'])
 def login():
@@ -67,12 +114,29 @@ def login():
             'userId': id_receive,
             'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=100)
         }
+        # userId = id_receive
         token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
 
         # token을 줍니다.
         return jsonify({'result': 'success', 'token': token})
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+@app.route('/users/isAuth', methods=['GET'])
+def api_valid():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        # token을 시크릿키로 디코딩합니다.
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        # payload 안에 id가 들어있습니다. 이 id로 유저정보를 찾습니다.
+        userinfo = db.users.find_one({'userId': payload['userId']}, {'_id': 0})
+        return jsonify({'result': 'success', 'myname': userinfo['name']})
+    except jwt.ExpiredSignatureError:
+        # 위를 실행했는데 만료시간이 지났으면 에러가 납니다.
+        return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
+    except jwt.exceptions.DecodeError:
+        # 로그인 정보가 없으면 에러가 납니다!
+        return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
         
 #팀원 전체 조회
 @app.route('/user/get', methods=['GET'])
